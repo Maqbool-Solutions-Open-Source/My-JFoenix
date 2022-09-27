defa0eabd672b4701b53fa359e69870e067e4612
RationalityFrontline <rationalityfrontline@gmail.com>
2022-09-27 12:01:38 +0800

    version 17.0.5 Fix tickLine issue in JFXSpinnerSkin

--- jfoenix/build.gradle.kts
+++ jfoenix/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "org.rationalityfrontline.workaround"
-version = "17.0.4"
+version = "17.0.5"
 val NAME = project.name
 val DESC = "JavaFX Material Design Library"
 val GITHUB_REPO = "RationalityFrontline/JFoenix"
